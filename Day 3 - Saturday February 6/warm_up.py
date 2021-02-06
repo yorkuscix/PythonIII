d411652bf29324bdbf96403e10b7d7d13789db7e
yorkuscix <explore@yorku.ca>
2021-02-06 09:48:17 -0500

    updated day 3 starter code

--- Day 3 - Saturday February 6/warm_up.py
+++ Day 3 - Saturday February 6/warm_up.py
@@ -12,3 +12,7 @@ class Car:
     
     def __eq__(self, other):
         return self.licence_plate == other.licence_plate
+    
+    def __str__(self):
+        return ("This " + self.colour + " car has " + str(self.num_of_wheels) +
+                " wheels. Its licence plate is " + self.licence_plate + ".")
